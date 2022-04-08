cd25a042be54f9c39e437b7cc96ce99789881005
navidabdi <artsanaat@gmail.com>
2022-04-08 18:50:23 +0430

    fetch first data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const App = () => {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const [data, setdata] = useState({});
+  const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+  const fetchData = async () => {
+    const { data } = await axios.get(url);
+    setdata(data?.data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
+    <div className="App ">
       <h1>Hello World</h1>
+      {console.log(data)}
     </div>
   );
 };
