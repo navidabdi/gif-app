b21333a76997c981f14770f95d2ea3ad21ee8d6b
navidabdi <artsanaat@gmail.com>
2022-04-13 10:49:23 +0430

    add search section to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
 const Header = () => {
   return (
-    <header className="container py-3 border-b">
-      <a href="/" className="font-bold">
+    <header className="container flex gap-5 items-center justify-between py-5 mt-2 border-b border-blue-100">
+      <a href="/" className="font-bold text-xl tracking-widest">
         GIF FINDER
       </a>
+      <input
+        value={'test'}
+        className="flex-grow md:w-[50%] md:flex-grow-0 p-2 text-lg border-blue-100 border outline-none rounded-md focus:border-blue-400 focus:bg-blue-50"
+        type="text"
+        placeholder="Search"
+      />
     </header>
   );
 };
