36b369c3ea678ca95b93ae5dfaea9533aad7f403
navidabdi <artsanaat@gmail.com>
2022-04-13 10:35:45 +0430

    add user data to GifBox component

--- src/components/GifBox.tsx
+++ src/components/GifBox.tsx
@@ -3,15 +3,34 @@ import { useStateContext } from '../contexts/StateGif';
 const GifBox = ({ gif }: { gif: any }) => {
   // const [giphData, fetchData] = useStateContext();
   return (
-    <article className="p-3 shadow-sm rounded-sm bg-white">
+    <article className="p-4 shadow-sm rounded-sm bg-white flex flex-col gap-4">
       <img
         className="w-full h-full object-cover rounded-sm"
-        src={gif?.images?.downsized?.url}
+        src={gif?.images?.fixed_height?.webp}
         alt={gif?.title}
         loading="lazy"
       />
 
-      {/* {console.log(giphData?.images?.downsized?.url)} */}
+      <div className="flex gap-2 items-center">
+        {gif?.user?.avatar_url ? (
+          <img
+            className="object-cover w-10 h-10 rounded-full overflow-hidden border bg-blue-100"
+            src={gif?.user?.avatar_url}
+            alt={gif?.user?.username}
+          />
+        ) : (
+          <div className="object-cover w-10 h-10 flex justify-center items-center rounded-full overflow-hidden border bg-blue-100">
+            <p className="font-bold text-blue-500 text-lg">
+              {gif?.user?.username
+                ? gif?.user?.username[0].toUpperCase()
+                : ':)'}
+            </p>
+          </div>
+        )}
+        <p className="text-gray-600">
+          {gif?.user?.username ? gif?.user?.username : 'Unkown User'}
+        </p>
+      </div>
     </article>
   );
 };
--- src/components/GifBoxes.tsx
+++ src/components/GifBoxes.tsx
@@ -4,7 +4,7 @@ import GifBox from './GifBox';
 const GifBoxes = () => {
   const [giphData, fetchData] = useStateContext();
   return (
-    <section className="grid gap-4 container my-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <section className="grid gap-5 container my-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {giphData?.map((gif: any) => (
         <GifBox key={gif.id} gif={gif} />
       ))}
